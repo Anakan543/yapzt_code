fc3e1f3957dd7d79d4f373afd4b36b134eee2fa8
Anakan543 <117722031+Anakan543@users.noreply.github.com>
2025-02-12 19:10:31 +0200

    Add files via upload

---
+++ JPZT for laborotorna 5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace JPZT_for_laborotorna_5
+{
+    public class Program
+    {
+
+         public static int CalculateResult(bool par2, int par3)
+        {
+            int result;
+
+
+            switch (par3) {
+                case 47:
+                case 68:
+                case 90:
+                    result = par3;
+                    break;
+                default:
+                    result = 91;
+                    break;
+            }
+            int var = 0;
+            if (var < result)
+            {
+                var += 3;
+                result += var;
+            }
+                
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int result = CalculateResult(false, 90);
+            Console.WriteLine(result);
+        }
+    }
+}
